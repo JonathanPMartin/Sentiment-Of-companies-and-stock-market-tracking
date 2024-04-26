be1ae059a6443973e5a09f2e8eb8a1b94574770f
Jonathan <jonathan_P_Martin@outlook.com>
2024-04-26 22:42:58 +0100

    fixing an issue i spotted with graph generation and added stock info to the database

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
 from .DBcommands import *
 import requests
+import random
 from bs4 import BeautifulSoup
-import datetime
+import datetime as Datetime
 from datetime import datetime, timedelta
 import nltk 
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 startdate="2024-03-26"
 Apikeys=["e3fff7fc440542c9923ebfd16005136f","2a026f3d832d4ba892d4a598b75157f8","16c656e972334d27bb0b94f37605f4f9","d5db1012f44841cd8aedd9bf0f53ef90","71d65b2753fc4b75b03e5414650cb0c1","54de7376d5474ca0a6e7ec9ecd81ca26"]
 
@@ -64,7 +66,7 @@ def Sentimentclean(text): #determines the sentiment of any given source taking i
     return Return
 
 def one_month_before(): #determines the date one month before, must be used on the 28th of the month or before to avoid edge cases
-    current_datetime = datetime.now()
+    current_datetime = Datetime.now()
     current_month = current_datetime.month
     current_day = current_datetime.day
     current_year = current_datetime.year
@@ -89,13 +91,20 @@ def one_month_before(): #determines the date one month before, must be used on t
 
 """
 Example use case of bellow function
+
+RandColour="#"
+hexcolours=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+for i in range(0,6):
+    RandColour=RandColour+hexcolours[random.randint(0,15)]
+
 graph1={
     "Xlabel":'Label',
     "Title":"Graph1",
     "Ylabel":"Label",
     "plots":{
-        "Xplots":[[0,1,2,3,4,5],[1,2,3,4,5,6]],
-        "YPlots":[[0,1,2,3,4,5],[2,3,4,5,6,7]],
+        "Xplots":[[datetime(2024, 4, 26, 9, 30), datetime(2024, 4, 26, 10, 0), datetime(2024, 4, 26, 10, 30),
+             datetime(2024, 4, 26, 11, 0), datetime(2024, 4, 26, 11, 30), datetime(2024, 4, 26, 12, 0)]],
+        "YPlots":[[100, 110, 105, 115, 120, 125]],
         "Label":['label1','label2'],
         "colours":['red','blue']
     }
@@ -108,17 +117,19 @@ graph2={
         "Xplots":[[0,1,2,3,4,5],[1,2,3,4,5,6]],
         "YPlots":[[0,1,2,3,4,5],[1,2,3,4,5,6]],
         "Label":['label1','label2'],
-        "colours":['red','blue']
+        "colours":[RandColour,'blue']
     }
 }
 """
-def PlotGraphs(graph1,graph2): #takes two grphs and creates png files of the plots that those two would create
+def PlotGraphs(graph1,graph2):
     for i in range(0,len(graph1['plots']['Xplots'])):
         plt.plot(graph1['plots']['Xplots'][i], graph1['plots']['YPlots'][i], label=graph1['plots']['Label'][i], color=graph1['plots']["colours"][i])
     
     plt.title(graph1["Title"])
     plt.xlabel(graph1["Xlabel"])
     plt.ylabel(graph1["Ylabel"])
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
+    plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=1))
     plt.legend()
     plt.grid(True)
     plt.savefig('plot1_image.png')
@@ -128,10 +139,15 @@ def PlotGraphs(graph1,graph2): #takes two grphs and creates png files of the plo
     plt.title(graph2["Title"])
     plt.xlabel(graph2["Xlabel"])
     plt.ylabel(graph2["Ylabel"])
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
+    plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=1))
+    plt.legend()
+    plt.grid(True)
     plt.legend()
     plt.grid(True)
     plt.savefig('plot2_image.png')
     plt.clf()
+
 
 #https://api.twelvedata.com/time_series?symbol=AAPL&interval=15min&start_date=2020-01-01&end_date=2023-01-01&apikey=5eb91eed0cb149eaa54cb7acc41210ee&source=docs
 
@@ -238,6 +254,18 @@ def MassAddUrlDatatodb():
             UriAddToDB(values,company)
     return companies
 def stokcswithdata():
+    current_datetime = Datetime.now()
+    current_month = current_datetime.month
+    current_day = current_datetime.day
+    current_year = current_datetime.year
+    if current_month<10:
+        current_month="0"+str(current_month)
+    if current_day<10:
+        current_day="0"+str(current_day)
+    current_month=str(current_month)
+    current_day=str(current_day)
+    current_year=str(current_year)
+    currentdate=current_year+"-"+current_month+"-"+current_day
     compaines=[]
     Qry = "SELECT * FROM Urls"
     Result=query_db(Qry)
@@ -250,6 +278,28 @@ def stokcswithdata():
         Qry="SELECT * FROM Companies Where company='{}'".format(i)
         QryResult=query_db(Qry,one=True)
         stock=QryResult["stock"]
+        url=f"https://api.twelvedata.com/time_series?symbol={stock}&interval=15min&start_date={startdate}&end_date={currentdate}&apikey=5eb91eed0cb149eaa54cb7acc41210ee&source=docs"
+        res=requests.get(url)
+        open_page=res.json()
+        if "values" in open_page.keys():
+            values=open_page['values']
+            for j in values:
+                Data=j["datetime"].split(" ")
+                value=j["close"]
+                bigTime=Data[0].split("-")
+                smallTime=Data[1].split(":")
+                year=int(bigTime[0])
+                month=int(bigTime[1])
+                day=int(bigTime[2])
+                hour=int(smallTime[0])
+                Min=int(smallTime[1])
+                #f"INSERT INTO Urls (storyUrl, company, headline,storyYear,storyMonth,storyDay,storyHour,storyminute) VALUES ('{i[1]}','{company}', '{Headline}', {i[2][0]},{i[2][1]},{i[2][2]},{i[2][3]},{i[2][4]})"   
+                Testqry=f"SELECT * FROM stockvalue WHERE stock='{stock}' AND stockYear={year} AND stockMonth={month} AND stockDay={day}" 
+                responce=query_db(Testqry) 
+                if True: #requries table purge to work sucks but it is what it is
+                    qry=f"INSERT INTO stockvalue(stock,stockYear,stockMonth,stockDay,stockHour,stockminute,stockvalue) Values('{stock}',{year},{month},{day},{hour},{Min},{value})"
+                    CalQry = write_db(qry)
+        #https://api.twelvedata.com/time_series?symbol=AAPL&interval=15min&start_date=2020-01-01&end_date=2023-01-01&apikey=5eb91eed0cb149eaa54cb7acc41210ee&source=docs
     return compaines
 @app.route("/")
 def index():
@@ -257,6 +307,7 @@ def index():
     Main Page.
     """
     return stokcswithdata()
+    #return stokcswithdata()
     #return flask.render_template("index.html")
 
 @app.route("/collecturls")
